3fd6430dba735b323816fd0abc13e6a9d49a4b89
Wasmeyah <wasmya2013@gmail.com>
2021-01-03 21:34:38 +0300

    alllll dooone with the bouns
    swag

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,13 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let n = prompt(`what's your name lovley ?`)
+function sayHello() {
+    
+    console.log(n)
+
+}
+sayHello();
+if (n==``){
+    alert(`hello unknown person`)
+}else {
+   alert(`hello ${n}`)
+}
